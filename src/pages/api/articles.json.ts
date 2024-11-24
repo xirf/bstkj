d57cef3cb91e2a0a6a24b17a12a84f52bfe64370
xirf <55616872+xirf@users.noreply.github.com>
2024-11-25 01:00:45 +0700

    fix: respose type

--- src/pages/api/articles.json.ts
+++ src/pages/api/articles.json.ts
@@ -3,7 +3,16 @@ import { getCollection } from 'astro:content';
 
 
 export const GET: APIRoute = async ({ params, request }) => {
-    const articles =  await getCollection('articles');
+    let articles = await getCollection('articles');
+
+    articles = articles.map(a => {
+        return {
+            ...a.data,
+            slug: a.slug,
+            id: a.id,
+            body: a.body,
+        }
+    })
 
     return new Response(
         JSON.stringify(articles)
